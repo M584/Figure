fcbf41e128dbccb40cd681742720f8ce57b04966
Маргарита Ковалевская <5842_kmv@kcup.net>
2017-12-26 05:18:28 +0700

    First commit. Added IFigure.cs

---
+++ IFigure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IFigure
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        int X { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        int Y { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        int Z { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        double GetVolume();
+    }
+}
