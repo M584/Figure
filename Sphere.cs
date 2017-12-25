cc93dc060d33a757c2b881066b2b8dab5b0c0d0c
Маргарита Ковалевская <5842_kmv@kcup.net>
2017-12-26 05:20:50 +0700

    First commit. Added Sphere.cs

---
+++ Sphere.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Sphere: IFigure
+    {
+        /// <summary>
+        /// Координаты X
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Координаты Y
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Координаты Z
+        /// </summary>
+        public int Z { get; set; }
+
+
+        private double _radius;
+
+        /// <summary>
+        /// При вычислении радиуса...
+        /// </summary>
+        public double Radius { get
+            {
+                return _radius;
+            } set
+            {
+                if (value < 0.0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _radius = value;
+                }
+            } }
+
+        /// <summary>
+        /// Расчет объёма сферы.
+        /// </summary>
+        /// <returns></returns>
+        public double GetVolume()
+        {
+            return 3.0 / 4.0 * Math.PI * Radius * Radius * Radius;
+        }
+    }
+
+
+
+
+
+
